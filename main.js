753f2afceb4c62c29299d71cfd5043c87d599066
Petrik Diak <diak@petrik.hu>
2023-12-18 12:22:12 +0100

    A felhasználó képes számokat a listához adni.

---
+++ main.js
@@ -0,0 +1,22 @@
+let szamok = [];
+
+function szamHozzaad(){
+    const szamInput = document.getElementById('szamInput');
+    const szamlista = document.getElementById('szamlista');
+
+    const ujszam = parseFloat(szamInput.value);
+    if (!isNaN(ujszam)){
+        szamok.push(ujszam);
+        kiiratas();
+    }
+    else{
+        alert("Érvénytelen szám!");
+    }
+
+    szamInput.value = '';
+}
+
+function kiiratas(){
+    const szamlista = document.getElementById('szamlista');
+    szamlista.textContent = 'Számok: ' + szamok.join(', ');
+}
